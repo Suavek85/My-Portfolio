2c8feafac88ebc3ca6461bc6a64277495a373084
SNiemiec <slawekniemiec@gmail.com>
2019-06-13 22:31:41 +0200

    Updates

---
+++ precache-manifest.ab9a58dfc946a204685e89d30ff1cc86.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14d2c9c94a8c97a8654171b7925ba45",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "74edebd344f8beb340c3",
+    "url": "/My-Portfolio/static/css/main.e767a8bd.chunk.css"
+  },
+  {
+    "revision": "6405724b8721e239990a",
+    "url": "/My-Portfolio/static/js/2.d1f11933.chunk.js"
+  },
+  {
+    "revision": "74edebd344f8beb340c3",
+    "url": "/My-Portfolio/static/js/main.c90bfee4.chunk.js"
+  },
+  {
+    "revision": "b7d4bf292e75006047a4",
+    "url": "/My-Portfolio/static/js/runtime~main.f7066801.js"
+  },
+  {
+    "revision": "747de1400a971bc3db2c338033b7d7eb",
+    "url": "/My-Portfolio/static/media/about-img-gen.747de140.jpg"
+  },
+  {
+    "revision": "ac15587cb9d855a6f9a6aafdd408b87f",
+    "url": "/My-Portfolio/static/media/courses-img.ac15587c.jpg"
+  },
+  {
+    "revision": "796145117515123da1ce11702c79a222",
+    "url": "/My-Portfolio/static/media/new-annual-planner.79614511.PNG"
+  },
+  {
+    "revision": "6684c1d3a0dbbf7874a42331f9772db2",
+    "url": "/My-Portfolio/static/media/new-emoticon-app.6684c1d3.PNG"
+  },
+  {
+    "revision": "17e9f1d6336ae230a90996694cba236c",
+    "url": "/My-Portfolio/static/media/new-face-recognition.17e9f1d6.PNG"
+  },
+  {
+    "revision": "bce9bd1172b6d04a2b0cf3afd49d27e9",
+    "url": "/My-Portfolio/static/media/new-music-app.bce9bd11.PNG"
+  },
+  {
+    "revision": "6249cb2df0e82c55e502101465cb5b50",
+    "url": "/My-Portfolio/static/media/new-pig-game.6249cb2d.PNG"
+  },
+  {
+    "revision": "38a6c143df9e1cea31e529f6bca5e6ab",
+    "url": "/My-Portfolio/static/media/new-playgrounds.38a6c143.PNG"
+  },
+  {
+    "revision": "ea9b7c86cce7137d21a47f177a229e8f",
+    "url": "/My-Portfolio/static/media/new-robo-friends.ea9b7c86.PNG"
+  },
+  {
+    "revision": "8c6b2d2c4144c9bd86eb3947d38fc820",
+    "url": "/My-Portfolio/static/media/new-typing-game.8c6b2d2c.PNG"
+  },
+  {
+    "revision": "da7b25385ec86cde85732561ad9e2b94",
+    "url": "/My-Portfolio/static/media/new-white-villages.da7b2538.PNG"
+  },
+  {
+    "revision": "13078d3724fd881a36204970293c49d1",
+    "url": "/My-Portfolio/static/media/udemy1.13078d37.png"
+  }
+]);
