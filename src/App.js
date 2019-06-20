47358f08d32a100ad231788fb9e76a09cfc19570
SNiemiec <slawekniemiec@gmail.com>
2019-06-21 00:02:30 +0200

    Font changed, Redux lazy and Suspense added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Suspense, lazy } from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
 import MyProjects from "./components/Projects/MyProjects/MyProjects";
 import CoursesProjects from "./components/Projects/CoursesProjects/CoursesProjects";
 import WebCourses from "./components/WebCourses/WebCourses";
 import Footer from "./components/Footer/Footer";
+const Header = lazy(() => import("./components/Header/Header"));
 
 class App extends Component {
-  state = {};
-
   render() {
     return (
       <div className="App">
-        <Header />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+        </Suspense>
         <MyProjects />
         <CoursesProjects />
         <WebCourses />
