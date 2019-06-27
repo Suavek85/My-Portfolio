6007adbc971d0984e65fea515aa1dc1f58a216f7
SNiemiec <slawekniemiec@gmail.com>
2019-06-27 13:48:24 +0200

    Codepen link added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,14 +9,17 @@ const Footer = () => {
       <a href="mailto:slawekniemiec@gmail.com" class="contact__icon">
         <i class="fas fa-envelope-square" />
       </a>
+      <a href="https://github.com/Suavek85" class="contact__icon">
+        <i class="fab fa-github-square" />
+      </a>
+      <a href="https://codepen.io/suavek1010/pens/public/" class="contact__icon">
+      <i class="fab fa-codepen"></i>
+      </a>
       <a
         href="https://www.linkedin.com/in/slawomirniemiec/"
         class="contact__icon"
       >
         <i class="fab fa-linkedin" />
-      </a>
-      <a href="https://github.com/Suavek85" class="contact__icon">
-        <i class="fab fa-github-square" />
       </a>
     </section>
   );
